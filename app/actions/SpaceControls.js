9a17eb82391899583f1ed8da902726d4fb53d634
codyss <cody.schwarz@gmail.com>
2016-03-17 16:36:28 -0400

    for oscar

---
+++ app/actions/SpaceControls.js
@@ -0,0 +1,27 @@
+/*eslint consistent-return: 0, no-else-return: 0*/
+import { polyfill } from 'es6-promise';
+import request from 'axios';
+import md5 from 'spark-md5';
+import * as types from 'constants/index';
+
+polyfill();
+
+/*
+ * Utility function to make AJAX requests using isomorphic fetch.
+ * You can also use jquery's $.ajax({}) if you do not want to use the
+ * /fetch API.
+ * Note: this function relies on an external variable `API_ENDPOINT`
+ *        and isn't a pure function
+ * @param Object Data you wish to pass to the server
+ * @param String HTTP method, e.g. post, get, put, delete
+ * @param String endpoint
+ * @return Promise
+ */
+function makeTopicRequest(method, id, data, api='/topic') {
+  return request[method](api + (id ? ('/' + id) : ''), data);
+}
+
+export function addSheet() {
+  //make request to server to add a new sheet in the account. Get back the mongoID and then add a new sheet with that ID and link to that sheet
+  return
+}

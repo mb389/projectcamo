7228ef4ff363ef48dc45f4cb81bf4408b76f6f23
Cody Schwarz <Cody@Codys-MacBook-Pro.local>
2016-03-19 16:09:34 -0400

    space control styled

--- app/components/Sheet/ColumnOptions.jsx
+++ app/components/Sheet/ColumnOptions.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames/bind';
 import styles from 'css/components/table';
-import { SplitButton } from 'react-bootstrap';
+import { DropdownButton, Glyphicon, Dropdown } from 'react-bootstrap';
 import { MenuItem } from 'react-bootstrap';
 
 const cx = classNames.bind(styles);
@@ -10,9 +10,22 @@ export default class ColumnOptions extends Component {
 
 	render () {
 		return (
-	  	 	<SplitButton className={cx('thead')} bsStyle='default' title={this.props.data.name} key={this.props.data.id} id={`dropdown-${this.props.data.name}`}>
-				{generateMenuItems()};
-		    </SplitButton>
+				//  	<DropdownButton className={cx('thead')} bsStyle='default' title={this.props.data.name }
+				//  key={this.props.data.id} id={`dropdown-${this.props.data.name}`}>
+				// {generateMenuItems()}
+		    // </DropdownButton>
+				<Dropdown id="dropdown-custom-1">
+		      <Dropdown.Toggle noCaret className={cx('thead')}>
+		        {this.props.data.name}
+						<Glyphicon className={cx('columnCarrat')} glyph="chevron-down" />
+		      </Dropdown.Toggle>
+		      <Dropdown.Menu className={cx('columnMenu')}>
+		        <MenuItem eventKey="1">Action</MenuItem>
+		        <MenuItem eventKey="2">Another action</MenuItem>
+		        <MenuItem eventKey="3" active>Active Item</MenuItem>
+		        <MenuItem eventKey="4">Separated link</MenuItem>
+		      </Dropdown.Menu>
+		    </Dropdown>
 		);
 	}
 }
@@ -22,10 +35,8 @@ function generateMenuItems () {
 		<MenuItem eventKey="1" active>Sort</MenuItem>,
 		<MenuItem eventKey="2">Another action</MenuItem>,
 		<MenuItem eventKey="3">Active Item</MenuItem>,
-		<MenuItem divider />,
 		<MenuItem eventKey="4">Separated link</MenuItem>
 		];
 
 	return items;
 }
-

01186273eeed70ee9b8951ad9a1748c662a841ac
Cody Schwarz <cody.schwarz@gmail.com>
2016-03-20 18:57:56 -0400

    double click to edit sheet name, needs to be synced up with BE

--- app/components/SpaceControls/SheetTab.jsx
+++ app/components/SpaceControls/SheetTab.jsx
@@ -3,6 +3,7 @@ import classNames from 'classnames/bind';
 import styles from 'css/components/space-control';
 import { connect } from 'react-redux';
 import * as Actions from '../../actions/spacecontrols';
+import ContentEditable from 'react-contenteditable';
 
 
 const cx = classNames.bind(styles);
@@ -12,21 +13,36 @@ class SheetsTab extends Component {
   constructor(props) {
     super(props);
     this.showSheet = this.showSheet.bind(this);
+    this.toggleDisabled = this.toggleDisabled.bind(this);
     this.active = 'activeSheet';
+    this.disabledBool = true;
   }
 
   showSheet() {
     this.props.dispatch(Actions.getSheet(this.props.spaceId, this.props.sheet));
   }
 
+  toggleDisabled() {
+    this.disabledBool = !this.disabledBool;
+  }
+
   render() {
     this.active = this.props.sheetToShow &&
       this.props.sheetToShow.name === this.props.sheet ?
       'activeSheet' : '';
+    let disabledBool = true;
     return (
       <div onClick={this.showSheet}
+        onDoubleClick={this.toggleDisabled}
         className={cx('SheetTab', 'SheetButton', this.active)}
-      >{this.props.sheet}</div>
+      >
+      <ContentEditable
+          html={this.props.sheet}
+            // innerHTML of the editable div
+          disabled={this.disabledBool}     // use true to disable edition
+          onChange={this.editSpaceName} // handle innerHTML change
+        />
+      </div>
     );
   }
 }

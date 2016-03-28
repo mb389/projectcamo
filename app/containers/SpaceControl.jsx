79594d56790a2b6b08e82bfcd8f1627d3ce73185
elpenao <elpenao@gmail.com>
2016-03-28 15:17:11 -0400

    file ref updates for heroku

--- app/containers/SpaceControl.jsx
+++ app/containers/SpaceControl.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
-import SheetsBar from 'components/spacecontrols/SheetsBar';
-import MagicBar from 'components/spacecontrols/MagicBar';
+import SheetsBar from 'components/SpaceControls/SheetsBar';
+import MagicBar from 'components/SpaceControls/MagicBar';
 import Table from 'components/Sheet/Table';
 import * as Actions from '../actions/SpaceControls';
 import * as SheetActions from '../actions/sheet';

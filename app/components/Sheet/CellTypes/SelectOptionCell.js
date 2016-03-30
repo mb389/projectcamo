78137522bf7546ac0ddb28e7eeb8f4180a18ab1c
apackin <apackin@gmail.com>
2016-03-30 13:12:54 -0400

    select types

--- app/components/Sheet/CellTypes/SelectOptionCell.js
+++ app/components/Sheet/CellTypes/SelectOptionCell.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames/bind';
 import { updateCell} from 'actions/sheet';
 import styles from 'css/components/table';
-import { DropdownButton, Glyphicon, Dropdown, MenuItem } from 'react-bootstrap';
+import { Input, Glyphicon } from 'react-bootstrap';
 
 
 const cx = classNames.bind(styles);
@@ -14,31 +14,26 @@ export default class SelectOptionCell extends Component {
 	}
 
 
-	handleChange(e, ekey){
+	handleChange(e){
+    console.log(e.target.value);
 	  const { dispatch, cellKey, rowIdx, row } = this.props;
-    dispatch(updateCell(ekey, cellKey, rowIdx, null, []));
+    dispatch(updateCell(e.target.value, cellKey, rowIdx, null, []));
 	}
 
 	render () {
     function generateOptions(arr) {
       return arr.map((opt, idx) => {
-        return (<MenuItem key={idx} eventKey={opt}>
-                   {opt}
-                </MenuItem>)
+        return (<option className={cx('cellSelectOption')} key={idx} value={opt}>{opt}</option>)
       })
     }
 
-    return (<Dropdown id="dropdown-custom-1" 
-              onSelect={this.handleChange} 
-              className={cx('typeDropdown') + ' col-xs-12'}>
-              <Dropdown.Toggle noCaret className=' col-xs-12'>
-                {this.props.cell.data}
-                <Glyphicon className={cx('columnCarrat')} glyph="menu-down" />
-              </Dropdown.Toggle>
-              <Dropdown.Menu className={cx('columnMenu')}>
-                  {generateOptions(this.props.cell.selectOptions)}
-              </Dropdown.Menu>
-            </Dropdown>)
+    return (<Input type="select" 
+                  className={cx('cellSelect')}
+                  placeholder={this.props.cell.data}
+                  value={this.props.cell.data}
+                  onChange={this.handleChange}>
+                {generateOptions(this.props.cell.selectOptions)}
+            </Input>)
   }
 
 }

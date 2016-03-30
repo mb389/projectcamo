6c69b472c5dc85427e1772260cc11291cf8627f0
Michael Bushoy <mbushoy@gmail.com>
2016-03-30 15:26:06 -0400

    monkey

--- app/containers/About.jsx
+++ app/containers/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from 'css/components/about';
 import Navigation from 'containers/Navigation';
+import sadMonkey from 'images/achievement_monkey.png';
 
 const cx = classNames.bind(styles);
 
@@ -17,10 +18,15 @@ const About = props => {
     <div className={cx('about')}>
       <div className={cx('description')}>
         <h1></h1>
-        <img width="700px" height="auto" src="https://cdn.filestackcontent.com/RtKLuovRQBmEvxbC3JOs" alt="" className={cx('logo')} />
+        <img width="1200px" height="auto" src="https://cdn.filestackcontent.com/RtKLuovRQBmEvxbC3JOs" alt="" className={cx('logo')} />
+
+        <div className={cx('sadMonkey')}>
+        <img width="1000px" height="auto" src={sadMonkey} />
         </div>
-      </div>
+        </div>
+
       <div className={cx('footer')}>(c) SpaceBook, 2016</div>
+      </div>
     </div>
   );
 };

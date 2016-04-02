6c2e7398d50aef1c25cebe5e6ddb5289d9e2880d
elpenao <elpenao@gmail.com>
2016-04-02 13:23:34 -0400

    key

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -4,7 +4,7 @@ module.exports = {
   db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/camo',
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   google: {
-    clientID: process.env.GOOGLE_CLIENTID || '62351010161-eqcnoa340ki5ekb9gvids4ksgqt9hf48.apps.googleusercontent.com',
+    clientID: process.env.GOOGLE_CLIENTID || 'AIzaSyAoo94JWybbein9PXeiprYaHmKRizDhGcI'
     clientSecret: process.env.GOOGLE_SECRET || '6cKCWD75gHgzCvM4VQyR5_TU',
     callbackURL: process.env.GOOGLE_CALLBACK || "/auth/google/callback"
   }
